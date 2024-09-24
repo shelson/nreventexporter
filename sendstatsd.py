6bccc9e4a5d2fec1fcebf27227039e470260d107
Simon Helson <simon.helson@gmail.com>
2024-09-24 21:43:27 +1200

    adding quick statsd emitting script for testing

---
+++ sendstatsd.py
@@ -0,0 +1,27 @@
+import time
+import socket
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 8125
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+count = 0
+countmax = 10
+counter = 0
+
+while True:
+    count = 0
+    while count < countmax:
+        message = f"fake.metric.{count}:{countmax - count}|g|#env:prod,app:web,host:server1"
+        sock.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+        print(f"Sent: {message}")
+        
+        
+        count += 1
+        time.sleep(0.1)
+    message = f"fake.counter:{counter}|c|#env:prod,app:web,host:server1"
+    sock.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+    print(f"Sent: {message}")
+    counter += 10
+    time.sleep(30)
